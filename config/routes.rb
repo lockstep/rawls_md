18e3b4c5530feff8d609d2ea1b04b8d63633d6b0
Kirati Samattanares <kirati.samat@gmail.com>
2017-05-17 15:30:45 +0700

    Add index & show route to Group

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   devise_for :users
 
   unauthenticated :user do
-    root to: 'pages#landing'
+    root to: 'groups#index'
   end
+
+  resources :groups, only: %i[index show]
 end
